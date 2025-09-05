1fd25e0ff50d22c754cadf5d4c4c8a3c147c7deb
jorgedelamora <jorgedlmv@gmail.com>
2025-09-05 08:03:00 -0600

    migrated tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { vi, describe, test, expect } from "vitest";
 import App from "./App";
 import Button from "./components/Button";
 
@@ -8,36 +10,35 @@ test("renders Welcome message", () => {
 	expect(linkElement).toBeInTheDocument();
 });
 test("renders hello button", () => {
-	render(<App />)
+	render(<App />);
 	const button = screen.getByTestId("hello-button");
-	expect(button).toBeInTheDocument()
-})
+	expect(button).toBeInTheDocument();
+});
 describe("when the button is clicked", () => {
 	test("the handle click function is called", () => {
-		const mockOnclick = jest.fn()
-		render(<Button onClick={mockOnclick}/>)
+		const mockOnclick = vi.fn();
+		render(<Button onClick={mockOnclick} />);
 		const button = screen.getByTestId("hello-button");
-		fireEvent.click(button)
-		expect(mockOnclick).toHaveBeenCalled()
-	})
+		fireEvent.click(button);
+		expect(mockOnclick).toHaveBeenCalled();
+	});
 	test("the welcome message should appear", () => {
-		render(<App />)
+		render(<App />);
 		const button = screen.getByTestId("hello-button");
-		fireEvent.click(button)
+		fireEvent.click(button);
 		const helloMessage = screen.getByTestId("hello-message");
 		expect(helloMessage).toBeInTheDocument();
-	})
-})
+	});
+});
 describe("if the button is not clicked", () => {
 	test("the handle click function should not be called", () => {
-		const mockOnclick = jest.fn()
-		render(<Button onClick={mockOnclick}/>)
-		expect(mockOnclick).not.toHaveBeenCalled()
-	})
+		const mockOnclick = vi.fn();
+		render(<Button onClick={mockOnclick} />);
+		expect(mockOnclick).not.toHaveBeenCalled();
+	});
 	test("the welcome message should not appear", () => {
-		render(<App />)
+		render(<App />);
 		const helloMessage = screen.queryByTestId("hello-message");
 		expect(helloMessage).not.toBeInTheDocument();
-	})
-})
-
+	});
+});
